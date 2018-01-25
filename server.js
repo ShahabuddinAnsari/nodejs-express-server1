670e63f4494ec58d01547fbf5d345da497f86091
Shahabuddin Ansari <ansari.extc@gmail.com>
2018-01-25 23:53:41 +0530

    updated port

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
-
-var port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
 
 hbs.registerHelper('getCurrentYear', ()=> {
   return new Date().getFullYear();
@@ -55,6 +53,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
