684c5d004e3caeae01e45dd9893df290684d65f9
Shahabuddin Ansari <ansari.extc@gmail.com>
2018-01-26 00:37:16 +0530

    added project page

--- server.js
+++ server.js
@@ -46,6 +46,14 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
+    welcomeMessage: 'Welcome to my projects page!'
+  });
+});
+
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
